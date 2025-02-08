cd9e94ca63d47866cdb7df647e4929db317f64e9
Lucas Costa <englucascbmelo@gmail.com>
2025-02-08 17:58:43 -0300

    model da tabela de sal

--- backend/database/Models/SalDeAdicaoModels.cs
+++ backend/database/Models/SalDeAdicaoModels.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backend.Entities;
 
 namespace backend.database.Models
 {
-    public class SalDeAdicaoModels
+    public class SalDeAdicaoModels : ComponentInfos
     {
-        
+
     }
 }
