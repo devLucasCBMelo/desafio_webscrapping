87f237c633b3b8f7b39fb9081bfce14b37bf5e0a
Lucas Costa <englucascbmelo@gmail.com>
2025-02-08 20:09:50 -0300

    limpando arquivos de models

--- backend/Context/FoodContext.cs
+++ backend/Context/FoodContext.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using backend.database.Models;
 using backend.Entities;
 using backend.Models;
 using Microsoft.EntityFrameworkCore;
---
+++ backend/database/Models/ComponentModels.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Entities;
+
+namespace backend.Models
+{
+    public class AcidosGraxosMonoinsaturadosModels : ComponentInfos { }
+
+    public class AcidosGraxosPoliinsaturadosModels : ComponentInfos { }
+
+    public class AcidosGraxosSaturadosModels : ComponentInfos { }
+
+    public class AcidosGraxosTransModels : ComponentInfos { }
+
+    public class AcucarDeAdicaoModels : ComponentInfos { }
+    public class AlcoolModels : ComponentInfos { }
+
+    public class CalcioModels : ComponentInfos { }
+    public class CarboidradoDisponivelModels : ComponentInfos { }
+
+    public class CarboidratoTotalModels : ComponentInfos { }
+
+    public class CinzasModels : ComponentInfos { }
+
+    public class CobreModels : ComponentInfos { }
+
+    public class ColesterolModels : ComponentInfos { }
+
+    public class EnergiaKcalModels : ComponentInfos { }
+
+    public class EnergiaKJModels : ComponentInfos { }
+
+    public class EquivalenteDeFolatoModels : ComponentInfos { }
+
+    public class FerroModels : ComponentInfos { }
+
+    public class FibraAlimentarModels : ComponentInfos { }
+
+    public class FosforoModels : ComponentInfos { }
+
+    public class LipidiosModels : ComponentInfos { }
+
+    public class MagnesioModels : ComponentInfos { }
+
+    public class ManganesModels : ComponentInfos { }
+
+    public class NiacinaModels : ComponentInfos { }
+
+    public class PotassioModels : ComponentInfos { }
+
+    public class ProteinaModels : ComponentInfos { }
+
+    public class RiboflavinaModels : ComponentInfos { }
+
+    public class SalDeAdicaoModels : ComponentInfos { }
+
+    public class SelenioModels : ComponentInfos { }
+
+    public class SodioModels : ComponentInfos { }
+
+    public class TiaminaModels : ComponentInfos { }
+
+    public class UmidadeModels : ComponentInfos { }
+
+    public class VitaminaARAEModels : ComponentInfos { }
+
+    public class VitaminaAREModels : ComponentInfos { }
+
+    public class VitaminaB6Models : ComponentInfos { }
+
+    public class VitaminaB12Models : ComponentInfos { }
+
+    public class VitaminaCModels : ComponentInfos { }
+
+    public class VitaminaDModels : ComponentInfos { }
+
+    public class VitaminaEModelsModels : ComponentInfos { }
+
+    public class ZincoModels : ComponentInfos { }
+
+}
