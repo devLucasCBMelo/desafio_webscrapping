0965a20287466d3cdf3ae6e15c79c530815385c8
Lucas Costa <englucascbmelo@gmail.com>
2025-02-12 16:32:24 -0300

    limpando a controller

--- backend/Controllers/ComponentController.cs
+++ backend/Controllers/ComponentController.cs
@@ -39,56 +39,5 @@ namespace backend.Controllers
 
             return Ok(resultado);
         }
-
-        [HttpPost("adicionar")]
-        public async Task<IActionResult> AdicionarEnergiaKJ([FromBody] ComponenteAlimento componente)
-        {
-            if (componente == null)
-            {
-                return BadRequest(new { Message = "Dados do componente não podem ser vazios" });
-            }
-
-            var energia = new EnergiaKJModels
-            {
-                Unidades = componente.Unidades,
-                ValorPor100g = componente.ValorPor100g,
-                ColherSopaCheia45g = componente.ColherSopaCheia45g,
-                CopoAmericanoDuplo200ml = componente.CopoAmericanoDuplo200ml,
-                CopoAmericanoPequeno130ml = componente.CopoAmericanoPequeno130ml,
-                PedacoUnidadeFatia = componente.PedacoUnidadeFatia,
-                PratoFundo450g = componente.PratoFundo450g,
-                PratoRaso350g = componente.PratoRaso350g
-            };
-
-            /* await _energiaService.AdicionarEnergia(energia);
-            return CreatedAtAction(nameof(AdicionarEnergiaKJ), new { codigo = energia.Cogido }, energia); */
-            return Ok(energia);
-        }
-
-        /* [HttpPost("AdicionarAlcool")]
-        public async Task<IActionResult> AdicionarAlcool([FromBody] ComponenteAlimento componente)
-        {
-            if (componente == null)
-            {
-                return BadRequest(new { Message = "Dados do componente não podem ser vazios" });
-            }
-
-            var alcool = new AlcoolModels
-            {
-                Cogido = componente.Cogido,
-                Unidades = componente.Unidades,
-                ValorPor100g = componente.ValorPor100g,
-                ColherSopaCheia45g = componente.ColherSopaCheia45g,
-                CopoAmericanoDuplo200ml = componente.CopoAmericanoDuplo200ml,
-                CopoAmericanoPequeno130ml = componente.CopoAmericanoPequeno130ml,
-                PedacoUnidadeFatia = componente.PedacoUnidadeFatia,
-                PratoFundo450g = componente.PratoFundo450g,
-                PratoRaso350g = componente.PratoRaso350g
-            };
-
-            Console.WriteLine(alcool);
-            await _alcoolService.AdicionarAlcool(alcool);
-            return CreatedAtAction(nameof(AdicionarAlcool), new { codigo = alcool.Cogido }, alcool);
-        } */
     }
 }
